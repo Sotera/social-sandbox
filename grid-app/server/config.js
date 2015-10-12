31eae2596d25fbf6476b30d2a63763fd25831afb
Justin Gawrilow <jgawrilow@PFI-C02J34CDDKQ5.local>
2015-10-12 10:34:03 -0400

    adding instagram key file

---
+++ grid-app/server/config.js
@@ -0,0 +1,10 @@
+config = {};
+
+
+// https://instagram.com/developer/clients/register/
+config.instagram_key = "put your client id here";
+
+// https://instagram.com/developer/authentication/
+config.instagram_access_token = "put your token here";
+
+module.exports = config;
