9592d453dfece611651a70d9e5aee1579136b8b4
Justin Gawrilow <jgawrilow@PFI-C02J34CDDKQ5.local>
2015-11-29 12:30:09 -0500

    adding client secret

--- grid-app/server/config.js
+++ grid-app/server/config.js
@@ -3,6 +3,8 @@ config = {};
 // https://instagram.com/developer/clients/register/
 config.instagram_key = "put your client id here";
 
+config.instagram_client_secret = "put your client secret here";
+
 // https://instagram.com/developer/authentication/
 config.instagram_access_token = "put your token here";
 
