9fd670df43f7fe997dda0467cdf54db4548282ae
Justin <justin.lueders@gmail.com>
2015-12-24 11:32:37 -0600

    Moving things around so it can all be run inside of a docker container via strongloop

--- server/config.template.js
+++ server/config.template.js
@@ -1,12 +1,12 @@
 config = {};
 
 // https://instagram.com/developer/clients/register/
-config.instagram_key = "080cb98cf5424d519352724b6048c5ee";//"put your client id here";
+config.instagram_key "put your client id here";
 
-config.instagram_client_secret = "e6a8c282722c4ba585aa9bb04199cac3";//"put your client secret here";
+config.instagram_client_secret = "put your client secret here";
 
 // https://instagram.com/developer/authentication/
-config.instagram_access_token = "1759912166.080cb98.42e22b0c49ab46f38a68170a85982908";//put your token here";
+config.instagram_access_token = "put your token here";
 
 // Memex elasticsearch
 config.es_path = 'http://localhost:9200';//'http://10.1.94.103:9200';
